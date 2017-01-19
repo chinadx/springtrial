3c256a78e2407a63c815fb0fefd49f4982130c15
Shaun Dong <chinadx@qq.com>
2017-01-19 16:58:41 +0800

    0

--- src/main/java/cn/goodman/business/user/controller/UserController.java
+++ src/main/java/cn/goodman/business/user/controller/UserController.java
@@ -27,18 +27,34 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * 列出所有用户信息
+     * @param request
+     * @param response
+     * @return
+     */
     @RequestMapping(method = RequestMethod.GET)
     public List<User> getUsers(HttpServletRequest request, HttpServletResponse response) {
         List<User> users = userService.getAllUsers();
         return users;
     }
 
+    /**
+     * 根据ID查找用户
+     * @param id
+     * @return
+     */
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public User getUserInfo(@PathVariable int id) {
         User user = userService.getUserInfo(id);
         return user;
     }
 
+    /**
+     * 增加一个用户
+     * @param record json格式形如 {"name":"黄鹤大舅","age":85,"password":"123456"}
+     * @return
+     */
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public User setUserInfo(@RequestBody User record) {
         userService.addUser(record);

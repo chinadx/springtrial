6b2d28614cb26485d396d1144e74783477f5675a
Shaun Dong <chinadx@qq.com>
2017-01-19 17:20:24 +0800

    增加static目录以提供默认的index页面

--- src/main/java/cn/goodman/business/main/controller/SampleController.java
+++ src/main/java/cn/goodman/business/main/controller/SampleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 public class SampleController {
-    @RequestMapping("/")
+    @RequestMapping("/sample")
     @ResponseBody
     String home() {
         return "Hello World!";

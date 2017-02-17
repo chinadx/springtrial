8823b29c7b0724324863809a6ce4961873bea55d
Shaun Dong <chinadx@qq.com>
2017-02-17 11:26:31 +0800

    1

--- src/test/java/cn/goodman/Hehe.java
+++ src/test/java/cn/goodman/Hehe.java
@@ -1,11 +1,16 @@
 package cn.goodman;
 
+import java.text.MessageFormat;
+import java.util.Date;
+
 /**
  * Created by Shaun on 2017/2/14.
  */
 public class Hehe {
     public static void main (String[] args) {
         String xx = "7ED56A3A91A443EA95DEF8FF55C8FE93";
-
+        String fmt = MessageFormat.format("{0}, a {1} destroyed the {2} town, {3} people died.",
+                new Date(), "blizzard storm", "South Park", 312);
+        System.out.println(fmt);
     }
 }

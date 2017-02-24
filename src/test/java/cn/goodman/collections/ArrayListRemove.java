e21cbfa9963d4626175ee4005b21615e61dc608a
Shaun Dong <chinadx@qq.com>
2017-02-24 09:45:07 +0800

    1

---
+++ src/test/java/cn/goodman/collections/ArrayListRemove.java
@@ -0,0 +1,30 @@
+package cn.goodman.collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Shaun on 2017/2/24.
+ */
+public class ArrayListRemove {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i=0; i<=100; i++) {
+            list.add(i);
+        }
+        System.out.println(list.toString());
+
+        Iterator<Integer> iter = list.iterator();
+        while(iter.hasNext()) {
+            Integer f = iter.next();
+            System.out.println(f);
+            if(f.intValue() % 4 == 0) {
+                System.out.println("删除" + f);
+                iter.remove();
+            }
+        }
+
+        System.out.println(list.toString());
+    }
+}

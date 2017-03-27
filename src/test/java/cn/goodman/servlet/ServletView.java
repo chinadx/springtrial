1b4d1e653792308d36b100c5dbc3852dd16d95f2
Shaun Dong <chinadx@qq.com>
2017-03-27 16:11:20 +0800

    1

---
+++ src/test/java/cn/goodman/servlet/ServletView.java
@@ -0,0 +1,128 @@
+package cn.goodman.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+/**
+ * Created by Shaun on 2017/3/27.
+ */
+public class ServletView extends HttpServlet {
+
+    private String message;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 设置响应内容类型
+        resp.setContentType("text/html");
+
+        // 实际的逻辑是在这里
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
+    @Override
+    protected long getLastModified(HttpServletRequest req) {
+        return super.getLastModified(req);
+    }
+
+    @Override
+    protected void doHead(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doHead(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doOptions(req, resp);
+    }
+
+    @Override
+    protected void doTrace(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doTrace(req, resp);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public String getInitParameter(String name) {
+        return super.getInitParameter(name);
+    }
+
+    @Override
+    public Enumeration<String> getInitParameterNames() {
+        return super.getInitParameterNames();
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return super.getServletConfig();
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return super.getServletContext();
+    }
+
+    @Override
+    public String getServletInfo() {
+        return super.getServletInfo();
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        message = "Hello World";
+    }
+
+    @Override
+    public void log(String msg) {
+        super.log(msg);
+    }
+
+    @Override
+    public void log(String message, Throwable t) {
+        super.log(message, t);
+    }
+
+    @Override
+    public String getServletName() {
+        return super.getServletName();
+    }
+
+}
